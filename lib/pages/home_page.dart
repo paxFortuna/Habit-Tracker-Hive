c018692d3c740c650ff128152d8bffa010c25f61
fortuna <avidia38@gmail.com>
2022-10-15 20:10:49 +0900

    refactor: ListView & checkBox method

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,39 +10,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // data structure for todays habit list
+  List todaysHabitList = [
+    // [ habitName, habitCompleted ]
+    ['Morning Run', false],
+    ['Read Book', false],
+    ['Code App', false],
+  ];
+
   // bool to control habit completed
   bool habitCompleted = false;
 
   // checkbox was tapped
-  void checkBoxTapped(bool? value) {
+  void checkBoxTapped(bool? value, int index) {
     setState(() {
-      habitCompleted = value!;
+      todaysHabitList[index][1] = value;
     });
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        child: ListView(
-          children: [
-            HabitTile(
-              habitName: 'Morning Run',
-              habitCompleted: habitCompleted,
-              onChanged: (value) => checkBoxTapped(value),
-            ),
-            HabitTile(
-              habitName: 'Morning Run',
-              habitCompleted: habitCompleted,
-              onChanged: (value) => checkBoxTapped(value),
-            ),
-            HabitTile(
-              habitName: 'Morning Run',
-              habitCompleted: habitCompleted,
-              onChanged: (value) => checkBoxTapped(value),
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: todaysHabitList.length,
+        itemBuilder: (context, index) {
+          return HabitTile(
+          habitName: todaysHabitList[index][0],
+          habitCompleted: todaysHabitList[index][1],
+          onChanged: (value) => checkBoxTapped(value, index),
+          );
+        },
+
       ),
     );
   }
