fc00880df34fe84408bde2cfe81300cea4c2596a
fortuna <avidia38@gmail.com>
2022-10-15 20:43:59 +0900

    feat: slidable

--- lib/components/habit_tile.dart
+++ lib/components/habit_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HabitTile extends StatelessWidget {
   final String habitName;
@@ -16,22 +17,48 @@ class HabitTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Container(
-        padding: const EdgeInsets.all(24),
-        decoration: BoxDecoration(
-          color: Colors.grey[100],
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            // checkbox
-            Checkbox(
-              value: habitCompleted,
-              onChanged: onChanged,
+            //settings option
+            SlidableAction(
+              onPressed: (context) {
+
+              },
+              backgroundColor: Colors.grey.shade800,
+              icon: Icons.settings,
+              borderRadius: BorderRadius.circular(12),
             ),
-            // habit name
-            Text(habitName),
+            // delete option
+            SlidableAction(
+              onPressed: (context) {
+
+              },
+              backgroundColor: Colors.red.shade300,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(12),
+            ),
           ],
+        ),
+        // startActionPane:,
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              // checkbox
+              Checkbox(
+                value: habitCompleted,
+                onChanged: onChanged,
+              ),
+              // habit name
+              Text(habitName),
+            ],
+          ),
         ),
       ),
     );
