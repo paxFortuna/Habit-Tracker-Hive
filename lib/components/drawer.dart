247b9212cfc7a55135243ebb059727c57f0f52be
fortuna <avidia38@gmail.com>
2022-10-17 15:12:02 +0900

    feat: omit comment

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 
 var defaultBackgroundColor = Colors.grey[300];
 
-//var myAppBar = AppBar(
-//  backgroundColor: Colors.grey[900],
-//);
-
 var myDrawer = Drawer(
   backgroundColor: Colors.grey[300],
   child: Column(
